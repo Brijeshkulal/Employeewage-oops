015dd63c8797e634cd4f156b3435f59070ae3661
“Brijesh” <brijeshkulal5@gmail.com>
2021-10-07 22:24:29 +0530

    Added daily employee wages

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -2,10 +2,13 @@ public class EmployeeWages {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 		int Is_Present = 1;
+		int wagePerHour = 20;
+		int workingHour = 8;
 		double Emp_Check = Math.floor(Math.random() * 10) % 2;
 		if(Emp_Check == Is_Present)
 		{
 			System.out.println("Employee is present");
+			System.out.println("Employee Wage per day is " + wagePerHour * workingHour);
 		}
 		else
     		{
