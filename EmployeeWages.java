37382d02b002b51278d750f13351e536e102df56
“Brijesh” <brijeshkulal5@gmail.com>
2021-10-07 23:46:15 +0530

    Added Methods to compute total wages

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -1,17 +1,21 @@
 public class EmployeeWages {
+	public static final int fullTime = 1;
+	public static final int partTime = 2;
+	public static final int wagePerHour = 20;
+	public static final int workingDays = 20;
+	public static final int maxHoursInMonth = 100;
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-		int fullTime = 1;
-		int partTime = 2;
-		int wagePerHour = 20;
-		int workingDays = 20;
-		int maxHoursInMonth = 100;
+		computeEmpWage();
+	}
+		
+	public static void computeEmpWage() {
 		int workingHour, totalEmployeeWage;
 		int totalWorkingHour = 0;
 		int totalWorkingDays = 0;
 		while(totalWorkingHour <= maxHoursInMonth && totalWorkingDays < workingDays)
 		{
-			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch(empCheck)
 			{
@@ -25,9 +29,10 @@ public class EmployeeWages {
 				workingHour = 0;
 			}
 			totalWorkingHour += workingHour;
+			totalWorkingDays++;
 			System.out.println("Day : " + totalWorkingDays + " Emp Hrs : " +totalWorkingHour);
 		}
 		totalEmployeeWage = totalWorkingHour * wagePerHour;
 		System.out.println("Total Employee wages " + totalEmployeeWage);
-	}
+	}	
 }
