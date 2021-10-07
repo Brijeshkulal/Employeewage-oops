daaf082983c1e9846d000742fcd91810385f97bd
“Brijesh” <brijeshkulal5@gmail.com>
2021-10-07 22:19:36 +0530

    Added Employee Attendance status

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -1,5 +1,16 @@
 public class EmployeeWages {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program on Master Branch")
+		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+		int Is_Present = 1;
+		double Emp_Check = Math.floor(Math.random() * 10) % 2;
+		if(Emp_Check == Is_Present)
+		{
+			System.out.println("Employee is present");
+		}
+		else
+    		{
+      			System.out.println("Employee is absent");
+    		}
+
 	}
 }
